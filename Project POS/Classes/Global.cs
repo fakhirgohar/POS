96bbac4fe744c2abb37ece9b81dd0251caf769eb
Fakhir <fakhirgohar@gmial.com>
2024-03-06 16:31:25 +0500

    latest push made on 6 March 2024

--- Project POS/Classes/Global.cs
+++ Project POS/Classes/Global.cs
@@ -14,7 +14,7 @@ namespace Project_POS.Classes
         //"Data Source=DESKTOP-TC3F7UH;Initial Catalog=Testing;Integrated Security=True" S
 
 
-        public static string ConnectionString = "Data Source=DESKTOP-TC3F7UH;Initial Catalog=Testing;Integrated Security=True";
+        public static string ConnectionString = "Data Source=SKYTERNE\\SQLEXPRESS;Initial Catalog=Testing;Integrated Security=True";
         public static SqlConnection Con = new SqlConnection();
         public static SqlTransaction tran;
         public static string InventoryReportPath = @"InventoryModule\Reports\";
